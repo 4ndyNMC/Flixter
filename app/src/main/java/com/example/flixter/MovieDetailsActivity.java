734b936d93c36ec120fc2906a2b77353f6c24d98
andy <tmgthemultigamer@gmail.com>
2022-06-03 16:39:04 -0700

    finished rounding images and added poster to details pages

--- app/src/main/java/com/example/flixter/MovieDetailsActivity.java
+++ app/src/main/java/com/example/flixter/MovieDetailsActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.example.flixter.models.Movie;
 
 import org.parceler.Parcels;
@@ -20,6 +24,7 @@ public class MovieDetailsActivity extends AppCompatActivity {
     TextView tvTitle;
     TextView tvOverview;
     RatingBar rbVoteAverage;
+    ImageView ivPoster;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +37,19 @@ public class MovieDetailsActivity extends AppCompatActivity {
         tvTitle = (TextView) findViewById(R.id.tvDetailsTitle);
         tvOverview = (TextView) findViewById(R.id.tvDetailsOverview);
         rbVoteAverage = (RatingBar) findViewById(R.id.rbVoteAverage);
+        ivPoster = (ImageView) findViewById(R.id.ivDetailsPoster);
         // set the title and the overview
         tvTitle.setText(movie.getTitle());
         tvOverview.setText(movie.getOverview());
         // set the rating bar
         float voteAverage = movie.getVoteAverage().floatValue();
         rbVoteAverage.setRating(voteAverage / 2.0f);
+        // set the poster
+        Glide.with(this)
+                .load(movie.getPosterPath())
+                .transform(new CenterCrop(), new RoundedCorners(25))
+                .override(440, 660)
+                .placeholder(R.drawable.flicks_movie_placeholder)
+                .into(ivPoster);
     }
 }
